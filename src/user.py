1dd29033caf3eaf0ffcbe59557c780d4f26e177b
axel-RODRIGUEZ <axel.rodriguez@laplateforme.io>
2026-02-10 11:46:27 +0100

    feat: Created delete_user method

--- src/user.py
+++ src/user.py
@@ -5,12 +5,12 @@ class User:
     def __init__(self, name_input):
         self.__name = name_input
         self.__save_id = None
-        self.__pokedex = {}
+        self.pokedex = {}
         self.json_file = "data/pokedex.json"
 
-        self.create_user()
+        self.create_user_save()
 
-    def create_user(self):
+    def create_user_save(self):
         data = {}
         
         if os.path.exists(self.json_file):
@@ -44,7 +44,21 @@ class User:
             json.dump(data, f, indent=4)
 
     def delete_user(self):
-        pass
+        if not os.path.exists(self.json_file):
+            return
+
+        with open(self.json_file, "r") as f:
+            data = json.load(f)
+
+        user_key = f"save_id_{self.__save_id}"
+        
+        if user_key in data:
+            del data[user_key]
+            with open(self.json_file, "w") as f:
+                json.dump(data, f, indent=4)
+            
+            self.__save_id = None
+            self.pokedex = {}
 
     def get_save_id(self):
         return self.__save_id
