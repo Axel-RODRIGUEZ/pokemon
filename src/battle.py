8376b12a0746f239f076abb86abc8192e0fffef3
axel-RODRIGUEZ <axel.rodriguez@laplateforme.io>
2026-02-10 17:19:26 +0100

    fix: fixing method choose_rdm_pokemon not working correctly and adding a save in pokedex.json to test.

--- src/battle.py
+++ src/battle.py
@@ -1,35 +1,53 @@
 from json import load, dump
 from random import randint
 from PIL import Image
-
+from pokemon import Pokemon
 class Battle:
     def __init__(self, user_pkm):
         self.__turn = 0
         self.user_pkm = user_pkm
         self.user_pokedex = self.get_user_pokedex()
         self.wild_pokemon = self.choose_rdm_pokemon()
-        self.weakness_ratios = self.get_weakness_ratios()
+        # self.weakness_ratios = self.get_weakness_ratios()
 
     def get_user_pokedex(self):
-        with open("data/pokedex.json", "r") as f:
-            self.user_pokedex = load(f)
+        try:
+            with open("data/pokedex.json", "r", encoding='utf-8') as f:
+                return load(f)
+        except FileNotFoundError:
+            return []
             
-    def get_weakness_ratios(self):
-        data = None
-        with open("data/types.json", "r") as f:
-            data = load(f[self.user_pkm.types]), load(f[self.wild_pokemon.types])
+    # def get_weakness_ratios(self):
+    #     data = None
+    #     with open("data/types.json", "r") as f:
+    #         data = load(f[self.user_pkm.types]), load(f[self.wild_pokemon.types])
 
-        self.weakness_ratios = data
+    #     self.weakness_ratios = data
 
     def choose_rdm_pokemon(self):
-        data = {}
-        rdm = randint(1, 151)
-
-        with open("data/pokemon.json", "r") as f:
+        rdm = randint(0, 150)
+        
+        with open("data/pokemon.json", "r", encoding='utf-8') as f:
             data = load(f)
-        for pkm in data:
-            if pkm[rdm] in data:
-                self.wild_pokemon = Pokemon(pkm["name"]["fr"], pkm["stats"]["hp"], pkm["stats"]["atk"], pkm["stats"]["def"], pkm["stats"]["vit"], pkm["types"], 0, Image.open(f"../assets/{str(rdm)}.png"))
+            
+            pkm = data[rdm] 
+            
+            try:
+                img = Image.open(f"../assets/{rdm + 1}.png")
+            except:
+                img = None
+
+            wild_pkm = Pokemon(
+                name=pkm["name"]["fr"],
+                max_hp=pkm["stats"]["hp"],
+                attack=pkm["stats"]["atk"],
+                defense=pkm["stats"]["def"],
+                speed=pkm["stats"]["vit"],
+                types=pkm["types"], 
+                sprite=img
+            )
+            
+            return wild_pkm
 
 
     def check_turn(self):
@@ -39,18 +57,24 @@ class Battle:
             elif self.user_pkm.speed < self.wild_pokemon.speed:
                 self.__turn = self.wild_pokemon
             else:
-                random = randint(1, 2)
-                if random == 1:
-                    self.__turn = self.user_pkm
-                else: 
-                    self.__turn = self.wild_pokemon
+                self.__turn = self.user_pkm if randint(1, 2) == 1 else self.wild_pokemon
         else:
-            if self.__turn == self.user_pkm:
-                self.__turn = self.wild_pokemon
-            else:
-                self.__turn = self.user_pkm
+            self.__turn = self.wild_pokemon if self.__turn == self.user_pkm else self.user_pkm
 
 
     # def assign_attack_multi(self):
     #     if 
     #     check1 = self.weakness_ratios[self.user_pkm.types]
+
+battle = Battle("test")
+
+mon_starter = Pokemon(
+    name="Dracaufeu", 
+    max_hp=100, attack=80, defense=70, speed=100, 
+    types=[{"name": "Feu"}] 
+)
+
+battle = Battle(mon_starter)
+
+print(f"Combat lancé contre : {battle.wild_pokemon.name}")
+print(f"HP du sauvage : {battle.wild_pokemon.max_hp}")
