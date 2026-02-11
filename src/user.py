ff5388e4abda3ba9aba1724ecf424813fb3c0674
axel-RODRIGUEZ <axel.rodriguez@laplateforme.io>
2026-02-11 16:00:30 +0100

    feat: Adding load_user method and attribute main_pokemon

--- src/user.py
+++ src/user.py
@@ -1,14 +1,21 @@
 from json import load, dump, JSONDecodeError
 from os import path
-
+from src.data_management import DataManagement
 class User:
-    def __init__(self, name_input):
+    def __init__(self, name_input:str):
+        self.data = DataManagement()
         self.__name = name_input
-        self.__save_id = None
-        self.pokedex = {}
+        self.pokedex = self.load_user()
         self.json_file = "data/pokedex.json"
+        self.main = self.check_main()
+        self.__save_id = self.create_user_save()  
 
-        self.create_user_save()
+    def check_main(self):
+        for pokemon in self.pokedex:
+            if pokemon.is_main == True:
+                self.main = pokemon
+            else:
+                self.main = None
 
     def create_user_save(self):
         data = {}
@@ -33,11 +40,11 @@ class User:
                     pass
 
         if existing_ids:
-            self.__save_id = max(existing_ids) + 1
+            save_id = max(existing_ids) + 1
         else:
-            self.__save_id = 1
+            save_id = 1
 
-        user_key = f"save_id_{self.__save_id}"
+        user_key = f"save_id_{save_id}"
         data[user_key] = {
             "name": self.__name,
             "pokemons": {}
@@ -50,7 +57,7 @@ class User:
         if not path.exists(self.json_file):
             with open(self.json_file, "w") as f:
                 dump("", f, indent=4)
-                print("Le fichier est vide.")
+                print("File cannot be deleted : Fresh file.")
 
         with open(self.json_file, "r") as f:
             data = load(f)
@@ -65,7 +72,15 @@ class User:
             self.__save_id = None
             self.pokedex = {}
         else:
-            print("Le joueur recherché n'existe pas")
+            print("The player you try to delete doesn't exist. Please try again.")
 
     def get_save_id(self):
         return self.__save_id
+    
+    def load_user(self):
+        pokedex = self.data.load_pokedex()
+        for i in pokedex:
+            if i.keys() == self.__save_id:
+                return i
+            else: 
+                return {}
