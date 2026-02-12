6ac0478c4f10afa02e0760822088bf378480bb50
arthur-georget <arthur.georget@laplateforme.io>
2026-02-12 11:47:06 +0100

    refactor(display): Display now abstract method mother of DisplayMainMenu

--- src/display.py
+++ src/display.py
@@ -1,29 +1,27 @@
-from pygame import display,Surface,time,font,Color
+from pygame import display,Surface,time,font,Color,image
+from os import path,pardir
 from src.button import Button
+from abc import ABC, abstractmethod
 
-class Display:
+BASE_DIR = path.dirname(path.abspath(__file__))
+
+class Display(ABC):
     def __init__(self, 
                  screen: Surface, 
                  fonts: tuple[font.Font, font.Font]):
-        self.__screen = screen
-        self.__fonts = fonts
-        self.__background = Surface((screen.get_width(),screen.get_height()))
-        self.__background.fill(Color("#8CD3FF"))
-
-    def display_menu(self, buttons: list = []):
-        self.__screen.blit(self.__background, (0, 0))
-        if len(buttons) > 0:    
-            for button in buttons:
-                self.__draw_button(button)
-        display.update()
-
-    def display_battle(self, buttons: dict):
+        self._screen = screen
+        self._fonts = fonts
+        self._background = Surface((screen.get_width(),screen.get_height()))
+        self._background.fill(Color("#8CD3FF"))
+    
+    @abstractmethod
+    def display(self, buttons: list = []):
         pass
 
-    def __draw_button(self, button: Button):
+    def _draw_button(self, button: Button):
         surface = Surface((button.widthheight))
         surface.fill(button.current_color)
         if button.text != "":
-            text_surf = self.__fonts[0].render(button.text, True, button.current_font_color)
+            text_surf = self._fonts[0].render(button.text, True, button.current_font_color)
             surface.blit(text_surf, text_surf.get_rect(center = surface.get_rect().center))
-        self.__screen.blit(surface, button.lefttop)
+        self._screen.blit(surface, button.lefttop)
