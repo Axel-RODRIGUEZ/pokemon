394adc6e00b54341c760dbfedc7e3a8e5a4332b0
arthur-georget <arthur.georget@laplateforme.io>
2026-02-17 15:43:14 +0100

    feat(pokedex): Now display pokemon type in display center when pokemon button hovered

--- src/DisplayPokedexMenu.py
+++ src/DisplayPokedexMenu.py
@@ -9,10 +9,13 @@ class DisplayPokedexMenu(Display):
         UI_IMAGES_PATH = path.join(self._BASE_DIR, pardir, "assets", "images", "ui")
         #self.__logo = image.load(path.join(UI_IMAGES_PATH, 'menu', 'logo.png'))
 
-    def update(self, buttons: list = []):
+    def update(self, buttons: list = [], pokemon_details: dict = {}):
         self._screen.blit(self._background, (0, 0))
         #self._screen.blit(self.__logo, (self.__logo.get_rect(center = self._background.get_rect().center)))
-        
+        if bool(pokemon_details): #Check if pokemon_details is empty
+            text_surf = self._fonts[0].render(pokemon_details["category"], True, (255,255,255))
+            self._screen.blit(text_surf, text_surf.get_rect(center = self._screen.get_rect().center))
+            
         if len(buttons) > 0:
             for button in buttons:
                 self._draw_button(button)
--- src/PokedexMenu.py
+++ src/PokedexMenu.py
@@ -18,12 +18,16 @@ class PokedexMenu(Ui):
 
     def run(self):
         pokedex_menu_display = DisplayPokedexMenu(self._screen, self._fonts)
+        pokemon_details = {}
         is_running = True
         while is_running:
             for current_event in event.get():
                 for button in self._buttons:
                     if button.rect.collidepoint(mouse.get_pos()):
                         button.hovered()
+                        for pokemon in self._user.pokedex:
+                            if str(pokemon["pokedex_id"]) == button.get_target_name():
+                                pokemon_details = pokemon
                     else:
                         button.avoided()
                 if self.__poke_list_area.collidepoint(mouse.get_pos()):
@@ -51,5 +55,5 @@ class PokedexMenu(Ui):
                 else:
                     pass
                 
-            pokedex_menu_display.update(self._buttons)
+            pokedex_menu_display.update(self._buttons, pokemon_details)
         return is_running
