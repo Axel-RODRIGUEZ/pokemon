306c088f408a9ef1d9e0ea17869f35ce3267b7f2
arthur-georget <arthur.georget@laplateforme.io>
2026-02-17 09:21:55 +0100

    feat(Pokedex): Pokemon list scroll now functionnal

--- src/PokedexMenu.py
+++ src/PokedexMenu.py
@@ -14,7 +14,7 @@ class PokedexMenu(Ui):
                  user: User, 
                  fonts: list[font.Font, font.Font]):
         Ui.__init__(self, screen, buttons, user, fonts)
-        self.__poke_list_rect = Rect((0,0),(400,731))
+        self.__poke_list_area = Rect((0,0),(400,731))
 
     def run(self):
         pokedex_menu_display = DisplayPokedexMenu(self._screen, self._fonts)
@@ -26,15 +26,16 @@ class PokedexMenu(Ui):
                         button.hovered()
                     else:
                         button.avoided()
-                if self.__poke_list_rect.collidepoint(mouse.get_pos()):
+                if self.__poke_list_area.collidepoint(mouse.get_pos()):
                     if current_event.type == MOUSEBUTTONDOWN:
-                        # Scroll
-                        if current_event.button == 4:
-                            #scroll_offset = max(0, scroll_offset - 1)
-                            print("+++")
+                        if current_event.button == 4: # Scroll
+                            for button in self._buttons[:-1]: #Ignore last element
+                                button.lefttop = button.lefttop[0], button.lefttop[1]+50
+                                button.rect.move_ip(0,50)
                         elif current_event.button == 5:
-                            #scroll_offset = min(max_scroll, scroll_offset + 1)
-                            print("---")
+                            for button in self._buttons[:-1]: #Ignore last element
+                                button.lefttop = button.lefttop[0], button.lefttop[1]-50
+                                button.rect.move_ip(0,-50)
                         else:
                             for button in self._buttons:
                                 if button.rect.collidepoint(mouse.get_pos()):
@@ -47,5 +48,8 @@ class PokedexMenu(Ui):
                 elif current_event.type == QUIT:
                     is_running = False
 
+                else:
+                    pass
+                
             pokedex_menu_display.update(self._buttons)
         return is_running
