d4714ec3fccd9bd4a9ce53a40d19bdd18d0fafe6
arthur-georget <arthur.georget@laplateforme.io>
2026-02-17 10:57:24 +0100

    feat(Button): Now Button class constructor can take bg_image and hover_bg_image pygame Surfaces as parameter

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
 
     BASE_DIR = path.dirname(path.abspath(__file__))
     FONT_PATH = path.join(BASE_DIR, "assets", "fonts", "LiberationSans-Regular.ttf")
+    UI_IMAGES_PATH = path.join(BASE_DIR, "assets", "images", "ui", "menu")
 
     pygame.init()
     pygame.mixer.init()
@@ -19,9 +20,19 @@ def main():
 
     user = User(is_new=False)
     menu = MainMenu(screen, 
-                    [Button("battle", (500,450), text="Lancer une partie"),
-                    Button("add_pokemon", (500,550), text="Ajouter un Pokémon"),
-                    Button("pokedex", (500,650), text="Accéder au Pokédex")], 
+                    [Button("battle", 
+                            (500,450), 
+                            text="Lancer une partie", 
+                            bg_image=pygame.image.load(path.join(UI_IMAGES_PATH, "button.png")), 
+                            hover_bg_image=pygame.image.load(path.join(UI_IMAGES_PATH, "hover_button.png"))),
+                    Button("add_pokemon", (500,550), 
+                           text="Ajouter un Pokémon", 
+                           bg_image=pygame.image.load(path.join(UI_IMAGES_PATH, "button.png")), 
+                           hover_bg_image=pygame.image.load(path.join(UI_IMAGES_PATH, "hover_button.png"))),
+                    Button("pokedex", (500,650), 
+                           text="Accéder au Pokédex", 
+                           bg_image=pygame.image.load(path.join(UI_IMAGES_PATH, "button.png")), 
+                           hover_bg_image=pygame.image.load(path.join(UI_IMAGES_PATH, "hover_button.png")))], 
                     user, 
                     fonts)
     menu.run()
