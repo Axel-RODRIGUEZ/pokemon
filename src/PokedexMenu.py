559dacecead9d2f10278de47e546a56448952957
arthur-georget <arthur.georget@laplateforme.io>
2026-02-27 11:16:04 +0100

    fix(pokedexmenu): reworked code linebreaks for better readability

--- src/PokedexMenu.py
+++ src/PokedexMenu.py
@@ -1,10 +1,9 @@
 from pygame import Surface,Rect,font,event,mouse,MOUSEBUTTONDOWN,QUIT, time
-
 from src.Ui import Ui
 from src.DisplayPokedexMenu import DisplayPokedexMenu
-
 from src.Button import Button
 from src.User import User
+
 
 class PokedexMenu(Ui):
     
@@ -15,11 +14,13 @@ class PokedexMenu(Ui):
                  fonts: list[font.Font, font.Font],
                  clock: time.Clock,
                  selection_mode: bool = False):
+        
         Ui.__init__(self, screen, buttons, fonts, clock)
-        self.__selection_mode = selection_mode
+
         self.__pokedex_menu_display = DisplayPokedexMenu(self._screen, self._fonts, selection_mode)
         self.__user = user
         self.__poke_list_area = Rect((0,0),(400,731))
+        self.__selection_mode = selection_mode
 
 
     def run(self):
@@ -34,18 +35,22 @@ class PokedexMenu(Ui):
                         for pokemon in self.__user.pokedex:
                             if str(pokemon["name"]["fr"]) == button.get_target_name():
                                 pokemon_details = pokemon
+
                     else:
                         button.avoided()
+
                 if self.__poke_list_area.collidepoint(mouse.get_pos()):
                     if current_event.type == MOUSEBUTTONDOWN:
                         if current_event.button == 4: # Scroll
                             for button in self._buttons[:-1]: #Ignore last element
                                 button.lefttop = button.lefttop[0], button.lefttop[1]+50
                                 button.rect.move_ip(0,50)
+
                         elif current_event.button == 5:
                             for button in self._buttons[:-1]: #Ignore last element
                                 button.lefttop = button.lefttop[0], button.lefttop[1]-50
                                 button.rect.move_ip(0,-50)
+
                         elif current_event.button == 1 and self.__selection_mode:
                             for button in self._buttons:
                                 if button.rect.collidepoint(mouse.get_pos()):
@@ -53,13 +58,16 @@ class PokedexMenu(Ui):
                                         if button.get_target_name() == pokemon["name"]["fr"]:
                                             if pokemon["ko"] == False:
                                                 return button.get_target_name()
+                                            
                                             else:
                                                 pass
+
                 elif current_event.type == MOUSEBUTTONDOWN:
                     for button in self._buttons:
                         if button.rect.collidepoint(mouse.get_pos()) and button.get_target_name() == "return":
                             if self.__selection_mode:
                                 return None
+                            
                             else:
                                 return is_running
                 
